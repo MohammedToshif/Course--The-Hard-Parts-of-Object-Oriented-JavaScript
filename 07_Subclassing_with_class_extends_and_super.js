b059734a03d11583a41d776e351a4e159ce66052
MohammedToshif <toshif92612@gmail.com>
2024-02-09 12:35:11 +0530

    new work

--- 07_Subclassing_with_class_extends_and_super.js
+++ 07_Subclassing_with_class_extends_and_super.js
@@ -23,4 +23,46 @@ const user2 = new userCreator("Tim", 4)
 
 user1.sayName()
 
+
+                    // (02)
+
+class paidUserCreator extends userCreator {
+    constructor(paidName, paidScore);
+    this.accountBalance = accountBalance;
+}                    
+increaseBalance () {
+    this.accountBalance++;
+}
+
+const paidUser1 = new paidUserCreator("Alyssa", 8, 25);
+
+paidUser1.increaseBalance();
+
+paidUser1.sayName();
                  
+
+
+            //----- (02) - (Creating a Subclass with extends)
+
+// subclassing in solution 4            
+// ES2015 class approach
+
+// same code - explain (some part)
+
+
+
+            //----- (03) - (Creating an object with a subclass)
+
+// subclassing in solution 4            
+// ES2015 class approach
+
+// same code - explain (some part)
+
+
+
+            //----- (04) - (Using super in a subclass constructor)
+
+// subclassing in solution 4            
+// ES2015 class approach
+
+// same code - explain (some part)            
